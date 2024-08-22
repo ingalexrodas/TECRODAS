c489c99fbcab440c9ac36362ff70864cdd88d7ba
Ing AlexRodas <ing.alexrodas@gmail.com>
2024-08-22 08:31:13 -0500

    SUbe y guarda y elimina Libros

--- sistema/settings.py
+++ sistema/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'libreria',
+        'USER': 'root',
+        'PASSWORD':'https://www.youtube.com/watch?v=ezIj71CX944',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
@@ -117,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'')
+MEDIA_URL='/imagenes/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
